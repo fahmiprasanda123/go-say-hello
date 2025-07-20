a61ff4c9d339ff880af79388c49a45609ff3467c
Fahmi <fahmiprasanda@gmail.com>
2025-07-20 18:45:36 +0700

    Update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fahmiprasanda123/go-say-hello
+module github.com/fahmiprasanda123/go-say-hello/v2
 
 go 1.24.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World! by @fahmiprasanda"
+func SayHello(name string) string {
+	return "Hello World! by @fahmiprasanda" + name
 }
